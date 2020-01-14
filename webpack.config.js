fa516239ec4de8b50c17be1faaccd85ebe4c6d9b
Dharmendra Sikarwar <dsikarwar@mast.com>
2020-01-14 16:30:17 +0530

    added webpack and created dev work flow

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        rules:[
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[new HtmlWebpackPlugin({
+        template: "./src/public/index.html"
+    })]
+}
